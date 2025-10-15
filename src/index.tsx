a1a1125c709150b0ef9878bcda85dfcda9a06ffc
Maya Krygina <krygina-m@mail.ru>
2025-10-15 19:22:10 +0300

    ver 1

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 
 import { App } from './components/app/App';
-
 import './styles/index.scss';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
